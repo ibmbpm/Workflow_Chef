aa815ea6f4734a38d8bbb007b3305d6f772349d8
Haitao <bjwhtao@cn.ibm.com>
2019-03-20 15:44:05 +0800

    Add Upgrade and Applyifix for Single node

--- singlenode/apply_ifix/baw_singlenode_apply_ifix.sh
+++ singlenode/apply_ifix/baw_singlenode_apply_ifix.sh
@@ -7,7 +7,7 @@
 #
 # IBM Business Automation Workflow Cookbook Project, https://github.com/IBM-CAMHub-Open/cookbook_ibm_workflow_multios
 #
-# This script work with IBM Business Automation Workflow Cookbook project to deploy IBM Business Automation Workflow Enterprise on a single host. 
+# This script work with IBM Business Automation Workflow Cookbook project to apply interim fix packs to IBM Business Automation Workflow Enterprise on a single host.
 
 # Topology
 # Single host: IBM Business Automation Workflow Enterprise - Deployment Manager and Custom Node, one cluster member.
@@ -32,33 +32,8 @@ Print_TopologyLogs () {
 # Upload all roles to the chef server
 Upload_Roles () {
   
-    knife role from file $BAW_CHEF_TEMP_DIR/$SNODE_ROLE_INSTALL_FILE || return 1
-    knife role from file $BAW_CHEF_TEMP_DIR/$SNODE_ROLE_UPGRADE_FILE || return 1
     knife role from file $BAW_CHEF_TEMP_DIR/$SNODE_ROLE_APPLYIFIX_FILE || return 1
-    knife role from file $BAW_CHEF_TEMP_DIR/$SNODE_ROLE_CONFIG_FILE || return 1
     knife role from file $BAW_CHEF_TEMP_DIR/$SNODE_ROLE_POSTDEV_FILE
-}
-
-# # Upload all roles to the chef server
-# Upload_ALL_Roles_From_Dir () {
-
-#     knife role from file $BAW_CHEF_TEMP_DIR/*
-# }
-
-######## Bootstrap first ########
-Bootstrap () {
-  # sequentia
-
-  local task_bootstraps=( )
-
-  knife bootstrap $SNODE_IP_ADDR -N $SNODE_ON_CHEF_SERVER -x $SNODE_ROOT_USERNAME -P "$SNODE_ROOT_PW" -y > $SNODE_LOG &
-  local TASK_SNODE_BOOTSTRAP=$!
-  readonly TASK_SNODE_BOOTSTRAP
-  task_bootstraps+=("$TASK_SNODE_BOOTSTRAP")
-  echo
-  echo "$(date -Iseconds), MTASK: $LOG_SNODE_NAME Bootstrap starts"
-
-  Monitor 0 "${task_bootstraps[*]}" "$LOG_SNODE_NAME Bootstrap"
 }
 
 
@@ -67,36 +42,14 @@ BAW_Single_Node_Installation_Start () {
   # sequential
 
   echo
-  echo "$(date -Iseconds), MTASK: $LOG_SNODE_NAME, there are 5 tasks to do: Installation, Upgrade, Applyifix, Configuration, POST Action"
+  echo "$(date -Iseconds), MTASK: $LOG_SNODE_NAME, there are 2 tasks to do: Applyifix, POST Action"
 
-  knife node run_list add $SNODE_ON_CHEF_SERVER "role[$SNODE_ROLE_INSTALL_NAME]" || return 1
-  knife vault update $BAW_CHEF_VAULT_NAME $BAW_CHEF_VAULT_ITEM -S "role:$SNODE_ROLE_INSTALL_NAME" -C "$SNODE_ON_CHEF_SERVER" -M client  2>/dev/null || { echo "Error when updating chef vault"; return 1; }
-  knife ssh "name:$SNODE_ON_CHEF_SERVER" -a ipaddress "sudo chef-client" -x $SNODE_ROOT_USERNAME -P "$SNODE_ROOT_PW" >> $SNODE_LOG &
-  local TASK_SNODE_INSTALL=$!
-  readonly TASK_SNODE_INSTALL
-  Monitor 0 "$TASK_SNODE_INSTALL" "$LOG_SNODE_NAME Installation(4 tasks left)" || return 1
-
-  knife node run_list add $SNODE_ON_CHEF_SERVER "role[$SNODE_ROLE_UPGRADE_NAME]" || return 1
-  knife vault update $BAW_CHEF_VAULT_NAME $BAW_CHEF_VAULT_ITEM -S "role:$SNODE_ROLE_UPGRADE_NAME" -C "$SNODE_ON_CHEF_SERVER" -M client || { echo "Error when updating chef vault"; return 1; }
-  knife ssh "name:$SNODE_ON_CHEF_SERVER" -a ipaddress "sudo chef-client" -x $SNODE_ROOT_USERNAME -P "$SNODE_ROOT_PW" >> $SNODE_LOG &
-  local TASK_SNODE_UPGRADE=$!
-  readonly TASK_SNODE_UPGRADE
-  Monitor 0 "$TASK_SNODE_UPGRADE" "$LOG_SNODE_NAME Upgrade(3 tasks left)" || return 1
-
-  knife node run_list add $SNODE_ON_CHEF_SERVER "role[$SNODE_ROLE_APPLYIFIX_NAME]" || return 1
+  knife node run_list set $SNODE_ON_CHEF_SERVER "role[$SNODE_ROLE_APPLYIFIX_NAME]" || return 1
   knife vault update $BAW_CHEF_VAULT_NAME $BAW_CHEF_VAULT_ITEM -S "role:$SNODE_ROLE_APPLYIFIX_NAME" -C "$SNODE_ON_CHEF_SERVER" -M client || { echo "Error when updating chef vault"; return 1; }
   knife ssh "name:$SNODE_ON_CHEF_SERVER" -a ipaddress "sudo chef-client" -x $SNODE_ROOT_USERNAME -P "$SNODE_ROOT_PW" >> $SNODE_LOG &
   local TASK_SNODE_APPLYIFIX=$!
   readonly TASK_ SNODE_APPLYIFIX
-  Monitor 0 "$TASK_SNODE_APPLYIFIX" "$LOG_SNODE_NAME Applyifix(2 tasks left)" || return 1
-
-
-  knife node run_list add $SNODE_ON_CHEF_SERVER "role[$SNODE_ROLE_CONFIG_NAME]" || return 1
-  knife vault update $BAW_CHEF_VAULT_NAME $BAW_CHEF_VAULT_ITEM -S "role:$SNODE_ROLE_CONFIG_NAME" -C "$SNODE_ON_CHEF_SERVER" -M client || { echo "Error when updating chef vault"; return 1; }
-  knife ssh "name:$SNODE_ON_CHEF_SERVER" -a ipaddress "sudo chef-client" -x $SNODE_ROOT_USERNAME -P "$SNODE_ROOT_PW" >> $SNODE_LOG &
-  local TASK_SNODE_CONFIG=$!
-  readonly  TASK_SNODE_CONFIG
-  Monitor 0 "$TASK_SNODE_CONFIG" "$LOG_SNODE_NAME Configuration(1 task left)" || return 1
+  Monitor 0 "$TASK_SNODE_APPLYIFIX" "$LOG_SNODE_NAME Applyifix(1 task left)" || return 1
 
   knife node run_list add $SNODE_ON_CHEF_SERVER "role[$SNODE_ROLE_POSTDEV_NAME]" || return 1
   knife ssh "name:$SNODE_ON_CHEF_SERVER" -a ipaddress "sudo chef-client" -x $SNODE_ROOT_USERNAME -P "$SNODE_ROOT_PW" >> $SNODE_LOG &
@@ -106,11 +59,9 @@ BAW_Single_Node_Installation_Start () {
 }
 
 
-######## Start the program ########
 BAW_Single_Nodes_Chef_Start () {
 
   Upload_Roles || return 1
-  Bootstrap || return 1
   Create_Chef_Vaults || return 1
   BAW_Single_Node_Installation_Start
 }
@@ -118,12 +69,12 @@ BAW_Single_Nodes_Chef_Start () {
 Main_Start () {
 
   echo
-  echo "Start to install and configure IBM Business Automation Workflow Enterprise on one single host."
+  echo "Start to apply interim fix packs tp IBM Business Automation Workflow Enterprise on one single host."
   echo
   echo "Starting at: $(date -Iseconds)"
   echo
   
-  Generate_Roles
+  Generate_Roles "apply_ifix"
 
   ######## Prepare logs for nodes #######
   # The name for SNode in log printing
@@ -166,14 +117,14 @@ MY_DIR=${0%/*}
 if [[ ! -d "$MY_DIR" ]]; then MY_DIR="$PWD"; readonly MY_DIR; fi
 #echo current Dir is $MY_DIR
 
-  . "$MY_DIR/../libs/utilities_script" &&
-  . "$MY_DIR/../libs/dynamic_roles_script"  &&
-  . "$MY_DIR/../libs/dynamic_roles_singlenode_script" &&
+  . "$MY_DIR/../../libs/utilities_script" &&
+  . "$MY_DIR/../../libs/dynamic_roles_script"  &&
+  . "$MY_DIR/../../libs/dynamic_roles_singlenode_script" &&
 
   # Reference to properties dir
   readonly BAW_CHEF_PROPERTIES_DIR="$MY_DIR"
   # ./baw_singlenode.properties
-  readonly BAW_CHEF_PROPERTIES_FILE="$BAW_CHEF_PROPERTIES_DIR/baw_singlenode_fresh_install.properties"
+  readonly BAW_CHEF_PROPERTIES_FILE="$BAW_CHEF_PROPERTIES_DIR/baw_singlenode_apply_ifix.properties"
   # Test if $BAW_CHEF_PROPERTIES_FILE exists 
   getValueFromPropFile $BAW_CHEF_PROPERTIES_FILE || return 1
 
@@ -182,13 +133,15 @@ if [[ ! -d "$MY_DIR" ]]; then MY_DIR="$PWD"; readonly MY_DIR; fi
   var_Workflow01_name=$(echo $var_Workflow01_FQDN | cut -d '.' -f1)
 
   # Reference to templates dir
-  readonly BAW_CHEF_TMPL_DIR=$MY_DIR/../templates
+  readonly BAW_CHEF_TMPL_DIR=$MY_DIR/../../templates
 
 ######## Prepare logs #######
 # define where to log
-readonly REQUESTED_LOG_DIR="/var/log/baw_chef_shell_log/singlenode/host_${var_Workflow01_name}/fresh_install"
+readonly REQUESTED_LOG_DIR="/var/log/baw_chef_shell_log/singlenode/host_${var_Workflow01_name}/apply_ifix"
 readonly LOG_DIR="$( Create_Dir $REQUESTED_LOG_DIR )"
 # echo "BAW LOG Dir created $LOG_DIR"
 readonly BAW_CHEF_LOG="${LOG_DIR}/Monitor_${var_Workflow01_name}.log"
 
+
+######## Start the program ########
  Main_Start 2>&1 | tee $BAW_CHEF_LOG
--- singlenode/upgrade/baw_singlenode_upgrade.sh
+++ singlenode/upgrade/baw_singlenode_upgrade.sh
@@ -7,7 +7,7 @@
 #
 # IBM Business Automation Workflow Cookbook Project, https://github.com/IBM-CAMHub-Open/cookbook_ibm_workflow_multios
 #
-# This script work with IBM Business Automation Workflow Cookbook project to deploy IBM Business Automation Workflow Enterprise on a single host. 
+# This script work with IBM Business Automation Workflow Cookbook project to upgrade IBM Business Automation Workflow Enterprise with fix packs on a single host. 
 
 # Topology
 # Single host: IBM Business Automation Workflow Enterprise - Deployment Manager and Custom Node, one cluster member.
@@ -32,33 +32,9 @@ Print_TopologyLogs () {
 # Upload all roles to the chef server
 Upload_Roles () {
   
-    knife role from file $BAW_CHEF_TEMP_DIR/$SNODE_ROLE_INSTALL_FILE || return 1
     knife role from file $BAW_CHEF_TEMP_DIR/$SNODE_ROLE_UPGRADE_FILE || return 1
     knife role from file $BAW_CHEF_TEMP_DIR/$SNODE_ROLE_APPLYIFIX_FILE || return 1
-    knife role from file $BAW_CHEF_TEMP_DIR/$SNODE_ROLE_CONFIG_FILE || return 1
     knife role from file $BAW_CHEF_TEMP_DIR/$SNODE_ROLE_POSTDEV_FILE
-}
-
-# # Upload all roles to the chef server
-# Upload_ALL_Roles_From_Dir () {
-
-#     knife role from file $BAW_CHEF_TEMP_DIR/*
-# }
-
-######## Bootstrap first ########
-Bootstrap () {
-  # sequentia
-
-  local task_bootstraps=( )
-
-  knife bootstrap $SNODE_IP_ADDR -N $SNODE_ON_CHEF_SERVER -x $SNODE_ROOT_USERNAME -P "$SNODE_ROOT_PW" -y > $SNODE_LOG &
-  local TASK_SNODE_BOOTSTRAP=$!
-  readonly TASK_SNODE_BOOTSTRAP
-  task_bootstraps+=("$TASK_SNODE_BOOTSTRAP")
-  echo
-  echo "$(date -Iseconds), MTASK: $LOG_SNODE_NAME Bootstrap starts"
-
-  Monitor 0 "${task_bootstraps[*]}" "$LOG_SNODE_NAME Bootstrap"
 }
 
 
@@ -67,36 +43,21 @@ BAW_Single_Node_Installation_Start () {
   # sequential
 
   echo
-  echo "$(date -Iseconds), MTASK: $LOG_SNODE_NAME, there are 5 tasks to do: Installation, Upgrade, Applyifix, Configuration, POST Action"
+  echo "$(date -Iseconds), MTASK: $LOG_SNODE_NAME, there are 3 tasks to do: Upgrade, Applyifix, POST Action"
 
-  knife node run_list add $SNODE_ON_CHEF_SERVER "role[$SNODE_ROLE_INSTALL_NAME]" || return 1
-  knife vault update $BAW_CHEF_VAULT_NAME $BAW_CHEF_VAULT_ITEM -S "role:$SNODE_ROLE_INSTALL_NAME" -C "$SNODE_ON_CHEF_SERVER" -M client  2>/dev/null || { echo "Error when updating chef vault"; return 1; }
-  knife ssh "name:$SNODE_ON_CHEF_SERVER" -a ipaddress "sudo chef-client" -x $SNODE_ROOT_USERNAME -P "$SNODE_ROOT_PW" >> $SNODE_LOG &
-  local TASK_SNODE_INSTALL=$!
-  readonly TASK_SNODE_INSTALL
-  Monitor 0 "$TASK_SNODE_INSTALL" "$LOG_SNODE_NAME Installation(4 tasks left)" || return 1
-
-  knife node run_list add $SNODE_ON_CHEF_SERVER "role[$SNODE_ROLE_UPGRADE_NAME]" || return 1
+  knife node run_list set $SNODE_ON_CHEF_SERVER "role[$SNODE_ROLE_UPGRADE_NAME]" || return 1
   knife vault update $BAW_CHEF_VAULT_NAME $BAW_CHEF_VAULT_ITEM -S "role:$SNODE_ROLE_UPGRADE_NAME" -C "$SNODE_ON_CHEF_SERVER" -M client || { echo "Error when updating chef vault"; return 1; }
   knife ssh "name:$SNODE_ON_CHEF_SERVER" -a ipaddress "sudo chef-client" -x $SNODE_ROOT_USERNAME -P "$SNODE_ROOT_PW" >> $SNODE_LOG &
   local TASK_SNODE_UPGRADE=$!
   readonly TASK_SNODE_UPGRADE
-  Monitor 0 "$TASK_SNODE_UPGRADE" "$LOG_SNODE_NAME Upgrade(3 tasks left)" || return 1
+  Monitor 0 "$TASK_SNODE_UPGRADE" "$LOG_SNODE_NAME Upgrade(2 tasks left)" || return 1
 
   knife node run_list add $SNODE_ON_CHEF_SERVER "role[$SNODE_ROLE_APPLYIFIX_NAME]" || return 1
   knife vault update $BAW_CHEF_VAULT_NAME $BAW_CHEF_VAULT_ITEM -S "role:$SNODE_ROLE_APPLYIFIX_NAME" -C "$SNODE_ON_CHEF_SERVER" -M client || { echo "Error when updating chef vault"; return 1; }
   knife ssh "name:$SNODE_ON_CHEF_SERVER" -a ipaddress "sudo chef-client" -x $SNODE_ROOT_USERNAME -P "$SNODE_ROOT_PW" >> $SNODE_LOG &
   local TASK_SNODE_APPLYIFIX=$!
   readonly TASK_ SNODE_APPLYIFIX
-  Monitor 0 "$TASK_SNODE_APPLYIFIX" "$LOG_SNODE_NAME Applyifix(2 tasks left)" || return 1
-
-
-  knife node run_list add $SNODE_ON_CHEF_SERVER "role[$SNODE_ROLE_CONFIG_NAME]" || return 1
-  knife vault update $BAW_CHEF_VAULT_NAME $BAW_CHEF_VAULT_ITEM -S "role:$SNODE_ROLE_CONFIG_NAME" -C "$SNODE_ON_CHEF_SERVER" -M client || { echo "Error when updating chef vault"; return 1; }
-  knife ssh "name:$SNODE_ON_CHEF_SERVER" -a ipaddress "sudo chef-client" -x $SNODE_ROOT_USERNAME -P "$SNODE_ROOT_PW" >> $SNODE_LOG &
-  local TASK_SNODE_CONFIG=$!
-  readonly  TASK_SNODE_CONFIG
-  Monitor 0 "$TASK_SNODE_CONFIG" "$LOG_SNODE_NAME Configuration(1 task left)" || return 1
+  Monitor 0 "$TASK_SNODE_APPLYIFIX" "$LOG_SNODE_NAME Applyifix(1 task left)" || return 1
 
   knife node run_list add $SNODE_ON_CHEF_SERVER "role[$SNODE_ROLE_POSTDEV_NAME]" || return 1
   knife ssh "name:$SNODE_ON_CHEF_SERVER" -a ipaddress "sudo chef-client" -x $SNODE_ROOT_USERNAME -P "$SNODE_ROOT_PW" >> $SNODE_LOG &
@@ -110,7 +71,6 @@ BAW_Single_Node_Installation_Start () {
 BAW_Single_Nodes_Chef_Start () {
 
   Upload_Roles || return 1
-  Bootstrap || return 1
   Create_Chef_Vaults || return 1
   BAW_Single_Node_Installation_Start
 }
@@ -118,12 +78,12 @@ BAW_Single_Nodes_Chef_Start () {
 Main_Start () {
 
   echo
-  echo "Start to install and configure IBM Business Automation Workflow Enterprise on one single host."
+  echo "Start to upgrade IBM Business Automation Workflow Enterprise with fix packs on one single host."
   echo
   echo "Starting at: $(date -Iseconds)"
   echo
   
-  Generate_Roles
+  Generate_Roles "upgrade_fixpack"
 
   ######## Prepare logs for nodes #######
   # The name for SNode in log printing
@@ -166,14 +126,14 @@ MY_DIR=${0%/*}
 if [[ ! -d "$MY_DIR" ]]; then MY_DIR="$PWD"; readonly MY_DIR; fi
 #echo current Dir is $MY_DIR
 
-  . "$MY_DIR/../libs/utilities_script" &&
-  . "$MY_DIR/../libs/dynamic_roles_script"  &&
-  . "$MY_DIR/../libs/dynamic_roles_singlenode_script" &&
+  . "$MY_DIR/../../libs/utilities_script" &&
+  . "$MY_DIR/../../libs/dynamic_roles_script"  &&
+  . "$MY_DIR/../../libs/dynamic_roles_singlenode_script" &&
 
   # Reference to properties dir
   readonly BAW_CHEF_PROPERTIES_DIR="$MY_DIR"
   # ./baw_singlenode.properties
-  readonly BAW_CHEF_PROPERTIES_FILE="$BAW_CHEF_PROPERTIES_DIR/baw_singlenode_fresh_install.properties"
+  readonly BAW_CHEF_PROPERTIES_FILE="$BAW_CHEF_PROPERTIES_DIR/baw_singlenode_upgrade.properties"
   # Test if $BAW_CHEF_PROPERTIES_FILE exists 
   getValueFromPropFile $BAW_CHEF_PROPERTIES_FILE || return 1
 
@@ -182,11 +142,11 @@ if [[ ! -d "$MY_DIR" ]]; then MY_DIR="$PWD"; readonly MY_DIR; fi
   var_Workflow01_name=$(echo $var_Workflow01_FQDN | cut -d '.' -f1)
 
   # Reference to templates dir
-  readonly BAW_CHEF_TMPL_DIR=$MY_DIR/../templates
+  readonly BAW_CHEF_TMPL_DIR=$MY_DIR/../../templates
 
 ######## Prepare logs #######
 # define where to log
-readonly REQUESTED_LOG_DIR="/var/log/baw_chef_shell_log/singlenode/host_${var_Workflow01_name}/fresh_install"
+readonly REQUESTED_LOG_DIR="/var/log/baw_chef_shell_log/singlenode/host_${var_Workflow01_name}/upgrade"
 readonly LOG_DIR="$( Create_Dir $REQUESTED_LOG_DIR )"
 # echo "BAW LOG Dir created $LOG_DIR"
 readonly BAW_CHEF_LOG="${LOG_DIR}/Monitor_${var_Workflow01_name}.log"
